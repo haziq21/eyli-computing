b8cf1b9be11fc859dd18e038726f52c4a19ef4a1
Haziq Hairil <haziqhairil21@gmail.com>
2022-03-05 22:56:48 +0800

    Complete Windi config

---
+++ windi.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "windicss/helpers";
+
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', "sans-serif"],
+      },
+    },
+  },
+});
