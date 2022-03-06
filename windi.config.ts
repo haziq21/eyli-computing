634bd4866bed0ad8cc3ddc272be824296b1d0bb8
Haziq Hairil <haziqhairil21@gmail.com>
2022-03-06 21:43:19 +0800

    Fix import in windi config

--- windi.config.ts
+++ windi.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "windicss/helpers";
+import { defineConfig } from "https://esm.sh/windicss/helpers";
 
 export default defineConfig({
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"IBM Plex Sans"', "sans-serif"],
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"IBM Plex Sans"', "sans-serif"],
+            },
+        },
     },
-  },
 });
